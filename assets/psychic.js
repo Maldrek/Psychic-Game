19032d79e3defb99dc55c88bcad4166e22f7d6f2
Maldrek <scott.salem@gmail.com>
2018-02-03 23:54:42 -0600

    set number of user guesses to 10. You're supposed to be psychic.

--- assets/psychic.js
+++ assets/psychic.js
@@ -6,7 +6,7 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 // Variables for the game (wins, losses, guesses the user has remaining, letters, etc)
 var wins = 0;
 var losses = 0;
-var guessesLeft = 15;
+var guessesLeft = 10;
 var letters = [];
 
 // User input instance
